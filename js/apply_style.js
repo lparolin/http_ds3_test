0af8446ccfeb6c22857a02c32d27ae3b36c5799d
Luca Parolini <lucapar@gmail.com>
2016-06-08 23:08:12 +0200

    Added some css from w3c

---
+++ js/apply_style.js
@@ -0,0 +1,4 @@
+//Apply w3 css style to different part of the page
+$('h1').addClass("w3-card-4 w3-indigo w3-padding-left");
+$('p').addClass("w3-padding-left");
+$('button').addClass("w3-btn w3-blue-grey");
